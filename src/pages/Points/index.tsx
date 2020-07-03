9fe7ef8a33e59e4263d659ece3fb2f56c8e0e680
Matheus Pimentel <matheus08.pimentel@gmail.com>
2020-07-03 15:59:53 -0300

    Create the connection with API: items

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -15,6 +15,7 @@ interface Item {
 
 const Points = () => {
   const [items, setItems] = useState<Item[]>([]);
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
   const navigation = useNavigation();
 
   useEffect(() => {
@@ -29,6 +30,18 @@ const Points = () => {
 
   function handleNavigateToDetail() {
     navigation.navigate('Detail');
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([ ...selectedItems, id ]);
+    }
   }
 
   return (
@@ -74,9 +87,17 @@ const Points = () => {
           contentContainerStyle={{ paddingHorizontal:20 }}
         >
           {items.map(item => (
-            <TouchableOpacity key={String(item.id)} style={styles.item} onPress={() => {}}>
+            <TouchableOpacity 
+              key={String(item.id)} 
+              style={[
+                styles.item,
+                selectedItems.includes(item.id) ? styles.selectedItem : {}
+              ]} 
+              onPress={() => handleSelectItem(item.id)}
+              activeOpacity={0.6}
+            >
               <SvgUri width={42} height={42} uri={item.image_url} />
-              <Text style={styles.itemTitle}>{item.name}</Text>
+              <Text style={styles.itemTitle}>{item.name }</Text>
             </TouchableOpacity>
           ))}
         </ScrollView>
